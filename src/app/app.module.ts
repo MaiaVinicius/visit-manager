7ea22ff0b159f070864f95970e74eab24121243f
Vinicius <viniciusmaia.tx@gmail.com>
2017-08-30 18:27:24 -0300

    added config page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,52 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {AddContactPage} from "../pages/add-contact/add-contact";
-import { Geolocation } from '@ionic-native/geolocation';
+import {Geolocation} from '@ionic-native/geolocation';
 import {NativeGeocoder} from "@ionic-native/native-geocoder";
 import {SQLite} from "@ionic-native/sqlite";
-import { RestapiService } from '../providers/restapi-service/restapi-service';
+import {RestapiService} from '../providers/restapi-service/restapi-service';
 import {Http, HttpModule} from "@angular/http";
+import {ConfigPage} from "../pages/config/config";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    TabsPage,
-    AddContactPage
-  ],
-  imports: [
-    HttpModule,
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    TabsPage,
-    AddContactPage
-  ],
-  providers: [
-    // Http,
-    Geolocation,
-    NativeGeocoder,
-    SQLite,
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    RestapiService
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        TabsPage,
+        AddContactPage,
+        ConfigPage
+    ],
+    imports: [
+        HttpModule,
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        TabsPage,
+        AddContactPage,
+        ConfigPage
+    ],
+    providers: [
+        // Http,
+        Geolocation,
+        NativeGeocoder,
+        SQLite,
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        RestapiService
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,28 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { HomePage } from '../home/home';
-import { AddContactPage } from '../add-contact/add-contact';
+import {HomePage} from '../home/home';
+import {AddContactPage} from '../add-contact/add-contact';
 import {RestapiService} from "../../providers/restapi-service/restapi-service";
+import {ConfigPage} from "../config/config";
 
 @Component({
-  templateUrl: 'tabs.html'
+    templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  contacts = false;
-  user;
-  visits = false;
-  tab1Root = HomePage;
-  tab2Root = AddContactPage;
+    contacts = false;
+    user;
+    visits = false;
+    tab1Root = HomePage;
+    tab2Root = AddContactPage;
+    tab3Root = ConfigPage;
 
-  constructor(public restapiService: RestapiService) {
-    this.user = 104718;
-    this.getVisits();
-  }
-  getVisits() {
-    this.restapiService.getVisits(this.user)
-      .then(data => {
+    constructor(public restapiService: RestapiService) {
+        this.user = 104718;
+        this.getVisits();
+    }
 
-        this.visits = data.visits;
-      });
-  }
+    getVisits() {
+        this.restapiService.getVisits(this.user)
+            .then(data => {
+
+                this.visits = data.visits;
+            });
+    }
 }
